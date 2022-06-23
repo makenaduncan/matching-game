fb2b34e73c0b41d3066ac94a37bde2f92e55c276
chasemoses <43392487+chasemoses@users.noreply.github.com>
2022-06-22 20:21:29 -0600

    Updated routes
    Added caseController.js
    
    Added the beginning to all route files, and added exports to all of them.

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,9 @@ const swaggerDocument = require('../swagger/swagger.json');
 router.use('/api-docs', swaggerUi.serve);
 router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
-
+router.use('/', require('./cases'));
+router.use('/', require('./game'));
+router.use('/', require('./score'));
 
 
 module.exports = router;
