c227376104fab1d332bae59b47a713920f6d3686
chasemoses <43392487+chasemoses@users.noreply.github.com>
2022-06-29 19:35:00 -0600

    Score Schema update
    Score Schema is complete.

--- models/scoreModel.js
+++ models/scoreModel.js
@@ -2,13 +2,12 @@ const mongoose = require('mongoose')
 
 const scoreSchema = new mongoose.Schema({
 
-    agencyName: String,
-    supervisorName: String,
-    phoneNumber: String,
-    email: String
+    ranking: Number,
+    username: String,
+    score: Number
 
 });
 
-const scores = mongoose.model('Agency', scoreSchema, "scores");
+const scores = mongoose.model('Score', scoreSchema, "scores");
 
 module.exports = scores;
