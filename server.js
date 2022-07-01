1caeefd43f1ba6ce0f9f9b98029ccc77a77e5cc2
chasemoses <43392487+chasemoses@users.noreply.github.com>
2022-06-30 22:59:21 -0600

    Add in URL Encoding server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get("/user", requiresAuth(), (req, res) => {
 
 // Use json and also require index file that will reference all other route files.
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true}));
 app.use('/', require('./routes/index'));
 
 mongodb.initDb((err) => {
