d4097dca4930cfcae7d867adab98be60a1f64d13
makena <mduncan18@byui.edu>
2022-07-09 22:27:16 -0600

    test practice code

---
+++ __tests__/unitTest.spec.js
@@ -0,0 +1,15 @@
+const request = require("supertest");
+describe("User Test Suite", () => {
+  it("test post /agencies endpoints", async () => {
+    const body = {
+      name: "Police Department",
+      supervisor: "Jason Borne",
+      phone: "000-000-0000",
+      email: "jborne@email.com",
+    };
+    const response = await request("https://fp-matching-game.herokuapp.com")
+      .post("/agencies")
+      .send(body);
+    expect(response.statusCode).toBe(302);
+  });
+});
