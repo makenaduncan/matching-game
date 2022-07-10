352a22c37dc6c60d9219d18325ce8d5fa9ab2ffc
makena <mduncan18@byui.edu>
2022-07-09 20:02:36 -0600

    base64 code change experiment

--- models/case.js
+++ models/case.js
@@ -1,24 +1,23 @@
-const mongoose = require('mongoose')
-const agencySchema = require('./agency')
+const mongoose = require("mongoose");
+const agencySchema = require("./agency");
 
 const caseSchema = new mongoose.Schema({
-
-    caseName: String,
-    summariedCaseDescription: String,
-    caseType: String,
-    victimPicture: String,
-    victimName: String,
-    victimAge: Number,
-    caseDate: Date,
-    location: String,
-    caseStatus: String,
-    websiteUrl: String,
-    agencyInformation: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Agency"
-    }
+  caseName: String,
+  summariedCaseDescription: String,
+  caseType: String,
+  victimPicture: { base64: String, imageFormat: String },
+  victimName: String,
+  victimAge: Number,
+  caseDate: Date,
+  location: String,
+  caseStatus: String,
+  websiteUrl: String,
+  agencyInformation: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Agency",
+  },
 });
 
-const cases = mongoose.model('Case', caseSchema, "cases");
+const cases = mongoose.model("Case", caseSchema, "cases");
 
 module.exports = cases;
