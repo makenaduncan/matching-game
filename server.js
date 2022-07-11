fc4a8a04807b9f004dd8b487c073644d23ee1c83
makena <mduncan18@byui.edu>
2022-07-11 08:30:07 -0600

    added code to fix base64 issue

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const mongodb = require("./db/mongoConnect");
 const { requiresAuth } = require("express-openid-connect");
 const port = process.env.PORT || 3000;
 const cors = require("cors");
-const User = require('./models/user');
+const User = require("./models/user");
 
 const { auth } = require("express-openid-connect");
 const { get } = require("mongoose");
@@ -15,15 +16,27 @@ const config = {
   secret: process.env.SECRET,
   baseURL: process.env.BASE_URL,
   clientID: process.env.CLIENT_ID,
-  issuerBaseURL: process.env.ISSUER_BASE_URL
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
 };
 
 // auth router attaches /login, /logout, and /callback routes to the baseURL
 app.use(auth(config));
 
-app.use(cors({
-  origin: ['https://kim-quirk.github.io/cold-case', 'http://localhost:8080']
-}))
+app.use(bodyParser.json({ limit: "50mb", extended: true }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 50000,
+  })
+);
+app.use(bodyParser.text({ limit: "200mb" }));
+
+app.use(
+  cors({
+    origin: ["https://kim-quirk.github.io/cold-case", "http://localhost:8080"],
+  })
+);
 
 // req.isAuthenticated is provided from the auth router
 app.get("/", (req, res) => {
@@ -31,35 +44,29 @@ app.get("/", (req, res) => {
 });
 
 app.use((req, res, next) => {
-
   // Find out if user is in the collection already.
   // If not, Create new user object based on OIDC and add to collection.
   // If they are in collection, update last login time and increment number of calls.
   // Set req.user equal to the whole user object of the colletion for the user.
-  
-  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  res.setHeader("Access-Control-Allow-Origin", "*");
   next();
-})
+});
 
 // Pulls user information from Auth0 and creates a profile
 app.get("/user", requiresAuth(), (req, res) => {
-
   try {
     res.send(JSON.stringify(req.oidc.user));
-
-  } catch(err) {
-    res.send(403).json({message: err.message});
+  } catch (err) {
+    res.send(403).json({ message: err.message });
   }
-  
 });
-
 
 // Use json and also require index file that will reference all other route files.
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
-app.use('/', requiresAuth(), require('./routes/index'));
+app.use(express.urlencoded({ extended: true }));
+app.use("/", requiresAuth(), require("./routes/index"));
 
 mongodb.connectDB();
 
-app.listen(port, () =>
-console.log(`listening on PORT ${port}`));
+app.listen(port, () => console.log(`listening on PORT ${port}`));
